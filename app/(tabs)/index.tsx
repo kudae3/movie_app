4239b7df1a4aaa43e79e873867551a563f1ec990
Kudae Sithu <kudaesithu2@gmail.com>
2025-11-30 13:26:28 +0630

    add SearchBar component and update index layout with background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      className="flex-1 justify-center items-center"
+      className="flex-1 bg-primary"
     >
-      <Text className="text-green-600 text-2xl font-bold">Hello World !</Text>
+      <Image source={images.bg} className="absolute w-full z-0" resizeMode="cover" />
+      <ScrollView 
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+          <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+          <View>
+            <SearchBar/>
+          </View>
+      </ScrollView>
     </View>
   );
 }
